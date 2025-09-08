ced6a1c940a108d9921e169283bcf5c1cfaac35e
rhehdgus8831 <rhehdgus8831@naver.com>
2025-09-08 10:16:56 +0900

    컨텍스트 데이터 소비하기

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import './App.css';
 import Meals from "./components/Meals/Meals";
 import Cart from './components/Cart/Cart.jsx';
 import {useState} from 'react';
+import CartContext from './context/cart-context.js';
 
 const App = () => {
+
+    const x = '하하호호';
 
     // 장바구니 모달을 여닫는 상태변수
     const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,11 +19,18 @@ const App = () => {
     const handleHideCart = () => setCartIsShown(false);
 
     return <>
+        {/* value 속성에 하위 컴포넌트들이 공유할 상태값들을 명시 */}
+        <CartContext.Provider value={{
+            cartName : '카트카트',
+            amount : 10,
+            isOpen : false,
+        }}>
         {cartIsShown && <Cart onCloes={handleHideCart}/>}
         <Header onShowCart={handleShowCart}/>
         <div id="main">
             <Meals />
         </div>
+        </CartContext.Provider>
     </>;
 };
 
