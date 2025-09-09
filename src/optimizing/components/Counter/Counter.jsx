9ac80d414ebb962a6c2eeb7da6e9244a559b05a9
rhehdgus8831 <rhehdgus8831@naver.com>
2025-09-09 10:12:59 +0900

    카운터 이력 쌓기에서 발생하는 렌더링 문제

--- src/optimizing/components/Counter/Counter.jsx
+++ src/optimizing/components/Counter/Counter.jsx
@@ -5,6 +5,7 @@ import MinusIcon from '../UI/Icons/MinusIcon.jsx';
 import PlusIcon from '../UI/Icons/PlusIcon.jsx';
 import CounterOutput from './CounterOutput.jsx';
 import {log} from '../../log.js';
+import CounterHistory from './CounterHistory.jsx';
 
 const isPrime = (number) => {
     log('Calculating if is prime number', 2, 'other');
@@ -27,15 +28,21 @@ const Counter = ({initialCount}) => {
     log('<Counter /> rendered', 1);
     const initialCountIsPrime = isPrime(initialCount);
 
-    const [counter, setCounter] = useState(initialCount);
+    // 카운티의 변화를 배열로 추적
+    const [counterChanges, setCounterChanges] = useState([initialCount]);
+
+    // const [counter, setCounter] = useState(initialCount);
 
     const decrementHandler = useCallback(() => {
-        setCounter((prevCounter) => prevCounter - 1);
+       setCounterChanges(prev => [-1, ...prev])
     }, []);
 
     const incrementHandler = useCallback(() => {
-        setCounter((prevCounter) => prevCounter + 1);
+        setCounterChanges(prev => [1, ...prev])
     }, []);
+
+    // 현재 카운트 로그배열의 총합
+    const totalCount = counterChanges.reduce((acc, curr) => acc + curr, 0);
 
     return (
         <section className="counter">
@@ -49,13 +56,15 @@ const Counter = ({initialCount}) => {
                     onClick={decrementHandler}>
                     Decrement
                 </IconButton>
-                <CounterOutput value={counter}/>
+                <CounterOutput value={totalCount}/>
                 <IconButton
                     icon={PlusIcon}
                     onClick={incrementHandler}>
                     Increment
                 </IconButton>
             </p>
+
+            <CounterHistory history={counterChanges}/>
         </section>
     );
 };
